44f0af1b1aaebde127f26ef3dc4a49e9ae0f7297
ZhengBoLe <Zhengbole@lexue100.com>
2025-07-28 11:07:07 +0800

    Chore: add 2

--- js/forbidclick.js
+++ js/forbidclick.js
@@ -2,9 +2,17 @@
 window.onload = function() {
     // 获取所有的a标签元素
     var links = document.querySelectorAll('a');
+    var buttons = document.querySelectorAll('button');
   
     // 为每个链接添加点击事件监听器
     links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        // 阻止链接默认行为，即不进行跳转
+        event.preventDefault();
+      });
+    });
+
+    buttons.forEach(function(link) {
       link.addEventListener('click', function(event) {
         // 阻止链接默认行为，即不进行跳转
         event.preventDefault();
