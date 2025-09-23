05cc6bf1f35ed60e6b022c1ac91ba4b92e85df75
ZhengBoLe <Zhengbole@lexue100.com>
2025-09-23 09:23:35 +0800

    Chore: xx

---
+++ product/darkest-dungeon-ii_files/sealapps_email_config.js
@@ -0,0 +1,7 @@
+/**
+ * 当前配置所属：正式服
+ * 本文件为c端脚本配置文件，非必要情况下不做修改，若有必须修改请务必确认当前文件为正式服还是测试服的配置
+ */
+const bisApiBaseUrl = "https://emailnoticeapi.sealapps.com/";
+const TrustooBISENV = "production";
+const TRUSTOO_BIS_BASE_URL = "https://bisapi.sealapps.com/";
